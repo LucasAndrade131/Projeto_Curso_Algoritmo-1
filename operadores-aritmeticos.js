5e6e2de444a0abf98b6a923c78f43fb5d6018e7b
Lucas Andrade <lucas.andrade131@outlook.com>
2023-05-15 10:20:45 -0300

    Primeiro commite do código

---
+++ operadores-aritmeticos.js
@@ -0,0 +1,25 @@
+/*
+Var
+// Seção de Declarações das variáveis 
+//   inteiro exp: 5, 7, 9 valores exp: 1.3 1.5 2.0
+   resultado, valor01, valor02, valor03:real
+
+Inicio
+// Seção de Comandos, procedimento, funções, operadores, etc... 
+   valor01 := 10
+   valor02 := 10
+   valor03 := 10
+   resultado := ((valor01 + valor02 + valor02) / valor03) % 2
+   escreval ("Resultado: ", resultado)
+
+Fimalgoritmo
+*/
+
+var resultado, valor01, valor02, valor03;
+valor01 = 10
+valor02 = 10
+valor03 = 10
+resultado = ((valor01 + valor02) * valor03) 
+
+alert("resultado: " + resultado)
+
